b365911fef18e51b509d502f0f1784b7bc9a2662
eliasescalante <eliasw.escalante@gmail.com>
2023-10-11 16:50:56 -0300

    agregue la base de datos a CursosApp y comente la url de proyecto porque daba error por ahora

--- blogCafe/CursosApp/models.py
+++ blogCafe/CursosApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    nacimiento = models.DateField()
+    gustos = models.CharField(max_length=40)
+
+
+    
+
+class Tutor(models.Model):
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+    edad = models.IntegerField()
+    email = models.EmailField()
+
+    
+
+class Curso(models.Model):
+    curso = models.CharField(max_length=40)
+    tutor = models.CharField(max_length=40)
+    cupo = models.IntegerField()
+    fecha = models.DateField()
+    
+    
