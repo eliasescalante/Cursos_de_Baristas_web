8ffd45694d417a848cb968c945e98a84a8cf421f
eliasescalante <eliasw.escalante@gmail.com>
2023-10-13 17:45:13 -0300

    arregle las urls de user

--- blogCafe/User/urls.py
+++ blogCafe/User/urls.py
@@ -6,6 +6,4 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login', views.login_request, name="Login"),
     path('register/', views.register, name="Register"),
-    path('logout/', LogoutView.as_view(template_name='User/logout.html'), name="Logout"),
-    path('edit/', views.edit, name="Edit")
 ]
