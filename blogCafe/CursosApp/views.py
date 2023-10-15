0172a6774bafa4e97620d4ed5f4599c1051b92b0
Daniela Erario <danielaerarioo@gmail.com>
2023-10-15 10:54:59 -0300

    Boton inscribirse con c/html

--- blogCafe/CursosApp/views.py
+++ blogCafe/CursosApp/views.py
@@ -26,16 +26,38 @@ def nosotros(request):
 def imprimir_cursos(request):
 
     cursos = Curso.objects.all()
-    
+
     context = {
         'cursos': cursos
     }
     return render(request, r"CursosApp\cursos.html",context)
 
 class CursoDetalle(DetailView):
-    
+
     model=Curso
     template_name='CursosApp/curso_detalle.html'
+
+
+#VISTAS PARA LOS CURSOS DE CAFE
+def filtrado(request):
+    return render(request,'CursosApp/cursoFiltrado.html')
+
+def arte(request):
+    return render(request,'CursosApp/cursoArte.html')
+
+def barista(request):
+    return render(request,'CursosApp/cursoBaristaArte.html')
+
+def expreso(request):
+    return render(request,'CursosApp/cursoExpreso.html')
+
+def infusores(request):
+    return render(request,'CursosApp/cursoInfusores.html')
+
+def tostado(request):
+    return render(request,'CursosApp/cursoTostado.html')
+
+
 
 
 
