6b8b2175e6a3d107db5d53a5d3e92e103425c01c
eliasescalante <eliasw.escalante@gmail.com>
2023-10-14 21:34:16 -0300

    arregle el formulario de la edicion del perfil

--- blogCafe/User/forms.py
+++ blogCafe/User/forms.py
@@ -4,16 +4,15 @@ from django.contrib.auth.forms import UserCreationForm, User, PasswordChangeForm
 
 class UserEditForm(UserCreationForm):
 
-    primer_nombre = forms.CharField(max_length=20, label='Nombre', widget=forms.TextInput(attrs={'class':'form-control'}))
-    apellido = forms.CharField(max_length=20, label='Apellido', widget=forms.TextInput(attrs={'class':'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    usuario = forms.CharField(max_length=20, label='Usuario', widget=forms.TextInput(attrs={'class':'form-control'}))
-    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(label='Repita Contraseña', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    
+    password = None
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=20, label='Nombre', widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=20, label='Apellido', widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(max_length=20, label='Usuario', widget=forms.TextInput(attrs={'class':'form-control'}))
+
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('email', 'username', 'first_name', 'last_name')
 
 
 class UserRegisterForm(UserCreationForm):
