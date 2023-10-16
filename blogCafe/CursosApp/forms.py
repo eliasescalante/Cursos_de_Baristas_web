c1aab3774a782ba742eb5c50df4f833bea01bc6a
Francisco <Franciscomezas320@gmail.com>
2023-10-15 23:19:54 -0300

    comentarios

--- blogCafe/CursosApp/forms.py
+++ blogCafe/CursosApp/forms.py
@@ -8,8 +8,8 @@ class CursoFormulario(forms.Form):
     tutor = forms.CharField(max_length = 40)
     cupo = forms.IntegerField()
     fecha = forms.DateField()
-    imagen = forms.ImageField(upload_to = "cursos",null = True)
-    descripcion = forms.CharField(max_length = 400, null = True)
+    # imagen = forms.ImageField(upload_to = "cursos",null = True)
+    # descripcion = forms.CharField(max_length = 400, null = True)
 
     def _str__(self):
         return self.nombre
