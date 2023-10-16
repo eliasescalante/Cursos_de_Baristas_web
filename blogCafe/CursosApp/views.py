a48badd4c70a10718f1fcbd0b5d379359d36ab4d
Francisco <Franciscomezas320@gmail.com>
2023-10-16 14:48:31 -0300

    comentarios aunque todavia no andan

--- blogCafe/CursosApp/views.py
+++ blogCafe/CursosApp/views.py
@@ -66,7 +66,7 @@ class Comentarios(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['mi_formulario'] = context['form']  # Renombrar el formulario para que coincida con tu plantilla
+        context['mi_formulario'] = context['form']  
         return context
 
 
